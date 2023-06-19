499954e28f0f42df51305e45ecb41aee7ef1f8e2
shristi-rks <shristi.n08@gmail.com>
2023-06-19 15:05:47 +0300

    http module

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-// const Person = require("./person");
-
-// const person1 = new Person("Shris", 36);
-
-// console.log(person1);
-
-const Logger = require("./logger");
-
-const logger = new Logger();
-
-logger.on('message', data => console.log('Called Listener', data));
-
-logger.log('Hello World');
-logger.log('Hi');
-logger.log('Hello');
+const http = require('http')
--- person.js
+++ person.js
@@ -12,3 +12,9 @@ class Person {
     }
     }
 module.exports = Person;
+
+// const Person = require("./person");
+
+// const person1 = new Person("Shris", 36);
+
+// console.log(person1);
