ec08ebdaf35baa54ae7be7e26f049ce1171e9073
shristi-rks <shristi.n08@gmail.com>
2023-06-19 14:59:42 +0300

    event module

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Node.js
 learning node.js
+learning material: Node.js crash course
+source: https://www.youtube.com/watch?v=fBNz5xF-Kx4
+
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const Person = require("./person");
+// const Person = require("./person");
 
-const person1 = new Person("Shris", 36);
+// const person1 = new Person("Shris", 36);
 
-console.log(person1);
+// console.log(person1);
+
+const Logger = require("./logger");
+
+const logger = new Logger();
+
+logger.on('message', data => console.log('Called Listener', data));
+
+logger.log('Hello World');
+logger.log('Hi');
+logger.log('Hello');
